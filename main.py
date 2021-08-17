fd708bc4bef48a474d42b262310f5a93ff9622d4
Dimand Edvard <edi.dimand@gmail.com>
2021-08-17 16:44:22 +0300

    Smile detection working, architecture to be made

--- main.py
+++ main.py
@@ -14,30 +14,31 @@ while True:
     if not frame_read:
         print('Could not read webcam')
         break
-
     # Changing frame to black and white for optimization
     frame_grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Detecting faces using the classifier loaded
+    # Detecting faces/smiles using the classifier loaded
     faces_found = face_classifier.detectMultiScale(frame_grayscale)
-    smiles_found = smile_clasisfier.detectMultiScale(frame_grayscale)
-
-    if len(faces_found):
-        print('Faces found', len(faces_found))
-
+    for (x, y, w, h) in faces_found:
         # Drawing a green rectangle around the face
-        for (x, y, w, h) in faces_found:
-            print(x, y, x+w, y+h)
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (100, 200, 50), 1)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (100, 200, 50), 3)
 
-    if len(smiles_found):
-        print('Faces found', len(smiles_found))
-        # Drawing a green rectangle around the smile
+        # Exracting rectangle with face from photo and grayscale it again
+        face = frame[y:y+h, x:x+w]
+        face_grayscale = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
 
-        for (x, y, w, h) in smiles_found:
-            print(x, y, x+w, y+h)
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (100, 200, 250), 1)
+        # Search for smile inside gray rectangle containing the face
+        smiles_found = smile_clasisfier.detectMultiScale(
+            face_grayscale, scaleFactor=1.1, minNeighbors=20)
 
+        for (x_, y_, w_, h_) in smiles_found:
+            # Draw rectangles around smiles inside a faces
+            cv2.rectangle(frame, (x+x_, y+y_), (x+x_+w_, y+y_+h_),
+                          (100, 200, 250), 1)
+
+        if len(smiles_found):
+            cv2.putText(frame, 'Smiling', (x, y+h+40),
+                        fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(100, 200, 250))
     cv2.imshow('Smile detector', frame)
     cv2.waitKey(1)
 
